e1800f8779a2959abb62300ca5b523548545b584
rahulrainaaa <rahulrainahome@live.com>
2019-02-04 04:33:35 +0530

    Initial commit

---
+++ app/src/main/java/demo/app/MainActivity.java
@@ -0,0 +1,40 @@
+package demo.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.map_view);
+        mMapView.setMap(new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16));
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.dispose();
+    }
+}
